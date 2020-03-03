09576ba9956b76a9227b32cea5a7bc79fb317bc5
EC2 Default User <ec2-user@ip-172-31-20-182.us-west-2.compute.internal>
2020-03-03 17:58:19 +0000

    update region for cognito

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-west-2_LoDbLiAfD', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '7bigj6ih99ccfd6rk07j6o3r62', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
